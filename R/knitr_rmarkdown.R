19ccc2cb8939b2ee329ff34f08db3177a49e3ecd
Avila-Herrera <avilaherrera1@llnl.gov>
2020-02-20 05:09:58 -0800

    Renames header walk

--- R/knitr_rmarkdown.R
+++ R/knitr_rmarkdown.R
@@ -1,20 +1,22 @@
 # Stuff to help with notebooks
 
 require(tidyverse)
+require(glue)
 
 #' Maps a function to a list and prints a "#" header for each element
 #'
 #' For use with results='asis' within an RMarkdown document
-iwalkh <- function(.x, .f, ..., level = 4, tabset = FALSE) {
-  ff <- function(f){
+hwalk <- function(.x, .f, ..., level = 4, tabset = FALSE) {
+  ff <- function(f) {
     force(f)
 
-    function(x, header, ...){
+    function(x, header, ...) {
       tbstr <- ifelse(tabset, " {.tabset .tabset-fade .tabset-pills}", "")
-      cat(glue(
-        '\n\n\n{strrep("#", level)} {header}{tbstr}\n\n', .trim = FALSE
+      cat(glue::glue(
+        '\n\n\n{strrep("#", level)} {header}{tbstr}\n\n',
+        .trim = FALSE
       ))
-      f(x, ...)
+      f(x, y = header, ...)
     }
   }
   .f <- as_mapper(.f, ...)
